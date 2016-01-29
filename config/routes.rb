e8b1ab5949abaf6b013213bcb77f91526b0c3a63
isodeveloper2 <jacob_latter@isodevelopers.com>
2016-01-29 16:25:11 -0500

    kkvdw

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   namespace :api, :defaults => {:format => :json} do
     namespace :v1 do
-      resources :bonnie_tyler_song_objects
+     resources :bonnie_tyler_song_objects
     end
   end
 
